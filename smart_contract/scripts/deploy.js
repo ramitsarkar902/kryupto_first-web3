62d96f12075f37221633e94d8e9d5919df125022
Ramit <ramitsarkar902@gmail.com>
2022-06-29 18:33:01 +0530

    started the contract

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,47 @@
+const { ethers, run, network } = require("hardhat");
+
+const main = async () => {
+  const transactionsFactory = await hre.ethers.getContractFactory(
+    "Transactions"
+  );
+  const transactionsContract = await transactionsFactory.deploy();
+
+  await transactionsContract.deployed();
+
+  console.log(`Deployed contract to: ${transactionContract.address}`);
+
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+};
+
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
---
+++ smart_contract/test/test-deploy.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Transactions", function () {
+  let transactionsFactory, transactionsContract;
+  beforeEach(async function () {
+    transactionsFactory = await ethers.getContractFactory("Transactions");
+    console.log("Deploying contract...");
+    transactionsContract = await transactionsFactory.deploy();
+  });
+
+  it("should deploy", async function () {
+    expect(transactionsContract.address).to.exist;
+  });
+
+  it("should add data to blockchain", async function () {
+    const tx = await transactionsContract.addToBlockChain(
+      "0x621611585965cb195a628ddbf631db6c50b3291c",
+      1000000000000000,
+      "hello test",
+      "keyword"
+    );
+    expect(tx.hash).to.exist;
+  });
+
+  it("should get data from blockchain", async function () {
+    const tx = await transactionsContract.getTransactionCount();
+    expect(tx).to.exist;
+  });
+
+  it("should not return an empty array", async function () {
+    const tx = await transactionsContract.getAllTransactions();
+    expect(tx).length.to.be.greaterThan(0);
+  });
+});
